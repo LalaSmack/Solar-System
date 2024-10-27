84f10e6ea0968df7ff032ea59a7442301f846293
Layal Singh <layal.singhkolkata@gmail.com>
2024-10-27 16:18:26 -0400

    basic scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import * as THREE from 'three';
 
 function App() {
+  useEffect(() => {
+  // Create a scene
+  const scene = new THREE.Scene();
+
+  // Create a camera, set position 
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1,10);
+  camera.position.z = 2;
+  
+  // Create  renderer
+  const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('threejs'), antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  renderer.render(scene, camera);
+})
   return (
     <div className="App">
-      <div class="font-bold text-5xl">
-        hello world
-      </div>
+      <canvas id="threejs"/>
     </div>
   )
 }
